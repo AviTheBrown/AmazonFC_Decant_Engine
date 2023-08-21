a483dd84f49322ca6c6244bfdd0faba9e3f28ce0
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2023-08-20 20:44:02 -0400

    created box and tote

---
+++ src/models/models.rs
@@ -0,0 +1,16 @@
+
+#[derive(Debug, Clone)]
+pub struct Box {
+	weight: u32,
+	id: String,
+	
+}
+
+
+#[derive(Debug, Clone)]
+pub struct Tote {
+	number_of_itesm: u32,
+	total_weight: u32,
+	id: String,
+}
+
