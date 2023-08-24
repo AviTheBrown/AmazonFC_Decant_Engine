7c2344122f5da4cb1a0ee5f6d8874a87d7192faa
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2023-08-24 02:34:51 -0400

    arbirtary.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 pub mod models;
+use std::rc::Rc;
 
-use models::{Container, MetaData, Tote};
+use models::{Container, ModelData, Tote};
 fn main() {
     let mut new_container = Tote::new();
     // new_container.weight = 30;
     println!("{}", new_container);
-
+    new_container.weight = 30;
     println!("{}", new_container)
 }
